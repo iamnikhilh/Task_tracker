d012e3a0eab9e0330cb77245912fc1552df1bf7e
Nikhil H <nikhilhalladamani3@gmail.com>
2025-06-23 00:57:53 +0530

    frontend link

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   
   // Enable CORS
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173' || 'https://task-tracker-theta-ivory.vercel.app',
     credentials: true,
   });
   
